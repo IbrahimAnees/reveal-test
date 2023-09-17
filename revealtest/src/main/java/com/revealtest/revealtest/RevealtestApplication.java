7211e72b0fc218f25d23d5cc42bb3b1c49491bf1
IbrahimAnees <ibrahimanees2002@gmail.com>
2023-09-18 08:53:51 +1200

    Added base springboot repo

---
+++ revealtest/src/main/java/com/revealtest/revealtest/RevealtestApplication.java
@@ -0,0 +1,13 @@
+package com.revealtest.revealtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevealtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevealtestApplication.class, args);
+	}
+
+}
---
+++ revealtest/src/test/java/com/revealtest/revealtest/RevealtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revealtest.revealtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevealtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
